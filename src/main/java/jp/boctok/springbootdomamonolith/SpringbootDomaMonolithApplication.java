c37dfcfad30e5936bee8382afbb27654453253e1
higashijima mastaka <boctok.ctpoba@gmail.com>
2022-11-12 11:15:23 +0900

    Initial commit

---
+++ src/main/java/jp/boctok/springbootdomamonolith/SpringbootDomaMonolithApplication.java
@@ -0,0 +1,13 @@
+package jp.boctok.springbootdomamonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDomaMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDomaMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/boctok/springbootdomamonolith/SpringbootDomaMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.boctok.springbootdomamonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDomaMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
